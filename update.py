cf4f4ffb35426099e42435233a483e521c894b0c
Shane <shane@smiskol.com>
2020-06-11 10:16:46 -0500

    fix

--- update.py
+++ update.py
@@ -7,7 +7,7 @@ def main():
   if not os.path.exists('/home/comma-dotfiles'):
     raise Exception('Please install to /home/comma-dotfiles!')
 
-  os.chdir('cd /home/comma-dotfiles')
+  os.chdir('/home/comma-dotfiles')
   r = subprocess.check_call(['git pull'])
   print(str(r))
   print('Updated successfully!')
