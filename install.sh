a6c6c1566107360565091595df17764076c92e0d
Scott Adair <scott@adair.cc>
2020-06-12 01:08:18 -0400

    fix a typo (#7)

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ fi
 cd ${OH_MY_COMMA_PATH}
 
 if [ ! -x "$(command -v powerline-shell)" ]; then
-  read -p "Do you want to install powerline? (y/n) [You will also need to install the fonts on your local terminal.]  >" choice
+  read -p "Do you want to install powerline? (y/n) [You will also need to install the fonts on your local terminal.]  >" choices
   case $choices in
     y|Y ) pip install powerline-shell;;
     * ) echo "Skipping...";;
