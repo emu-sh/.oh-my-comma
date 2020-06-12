adc7374b522b916df133a1a4df6ecdad5e09bff3
Alice Knag <AskAlice@users.noreply.github.com>
2020-06-11 19:12:40 -0700

    update with better install script/dir/etc (#6)
    * move existing commands to new syntax test
    
    fix
    
    what are these comments
    
    fix
    
    fix
    
    exit instead
    
    exit instead
    
    fix
    
    fix
    
    add update
    
    add pandaflash
    
    add pandaflash2
    
    add pandaflash2
    
    add debugging
    
    fix
    
    fix
    
    can we use multiline strings?
    
    cool, no need for \n
    
    okay, okay...
    
    only 1 indentation
    
    same indentation
    
    test
    
    test
    
    test
    
    test
    
    test
    
    ?
    
    ?
    
    ?
    
    this should work
    
    test
    
    fixes
    
    fixes
    
    add debugging_commands
    
    add debugging_commands
    
    use printf
    
    add reminder to reconnect
    
    reformat
    
    reminder
    
    test fork installer
    
    test fork installer
    
    change to /data first so if openpilot doesn't exist, we're at least in /data
    
    add documentation
    
    update readme
    
    fix documentation
    
    split up
    
    add newline
    
    rename dotfiles, could add dotfiles alias later, test loop to find free dir name for old openpilot folder
    
    better
    
    better
    
    better
    
    finish installfork function
    
    move commands out of function
    
    * fix README.md, and run through prettier
    
    * add editorconfig
    
    * code split
    
    * edit install/update
    
    * update update
    
    * update install
    
    * update install
    
    * update install script
    
    * update powerline install
    
    * update install
    
    * update powerline install
    
    * more tests
    
    * update install
    
    * update install script
    
    * update install script
    
    * update install
    
    * update emu-utils.sh
    
    * update install
    
    * test printf
    
    * update update
    
    * update install.sh
    
    * update utils
    
    * update install.sh
    
    * update update
    
    * update emu utils
    
    * update ln
    
    * update scripts
    
    * add powerline to src
    
    * update scripts
    
    * update install.sh
    
    * update
    
    * update bashrc
    
    * update readme
    
    Co-authored-by: Shane <shane@smiskol.com>

---
+++ emu-utils.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+SYSTEM_BASHRC_PATH=/home/.bashrc
+export COMMUNITY_PATH=/data/community
+export COMMUNITY_BASHRC_PATH=/data/community/.bashrc
+export OH_MY_COMMA_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source powerline.sh
+
+commands="
+  - update: updates this tool, requires restart of ssh session
+  - pandaflash: flashes panda
+  - pandaflash2: flashes panda without make recover
+  - debug: debugging tools
+  - installfork: Specify the fork URL after. Moves openpilot to openpilot.old"
+debugging_commands="
+  - controls: logs controlsd to /data/output.log"
+
+function _pandaflash() {
+  cd /data/openpilot/panda/board && make recover
+}
+
+function _pandaflash2() {
+  cd /data/openpilot/panda; pkill -f boardd; PYTHONPATH=..; python -c "from panda import Panda; Panda().flash()"
+}
+
+function _controlsdebug(){
+  pkill -f controlsd ; PYTHONPATH=/data/openpilot python /data/openpilot/selfdrive/controls/controlsd.py 2>&1 | tee /data/output.log
+}
+
+function _installfork(){
+  if [ $# -lt 1 ]; then
+    echo "You must specify a fork URL to clone!"
+    return 1
+  fi
+
+  old_dir="/data/openpilot.old"
+  old_count=0
+  if [ -d $old_dir ]; then
+    while [ -d "/data/openpilot.old.${old_count}" ]; do
+      old_count=$((old_count+1))  # counts until we find an unused dir name
+    done
+    old_dir="${old_dir}.${old_count}"
+  fi
+
+  echo "Moving current openpilot installation to ${old_dir}"
+  mv /data/openpilot ${old_dir}
+  echo "Fork will be installed to /data/openpilot"
+  git clone $1 /data/openpilot
+}
+
+function _debug(){
+  if [ $# -lt 1 ]; then  # verify at least two arguments
+    printf "You must specify a command for emu debug. Some options are:"
+    printf '%s\n' "$debugging_commands"
+    return 1
+  fi
+
+  if [ $1 = "controls" ]; then
+    _controlsdebug
+  else
+    printf "Unsupported debugging command! Try one of these:"
+    printf '%s\n' "$debugging_commands"
+  fi
+}
+
+function _updateohmycomma(){
+  source /data/community/.oh-my-comma/update.sh
+}
+
+function emu(){  # main wrapper function
+  if [ $# -lt 1 ]; then
+    printf "You must specify a command for emu. Some options are:"
+    printf '%s\n' "$commands"
+    return 1
+  fi
+
+  if [ $1 = "update" ]; then
+    _updateohmycomma
+  elif [ $1 = "pandaflash" ]; then
+    _pandaflash
+  elif [ $1 = "pandaflash2" ]; then
+    _pandaflash2
+  elif [ $1 = "installfork" ]; then
+    _installfork $2
+  elif [ $1 = "debug" ]; then
+    _debug $2
+  else
+    printf "Unsupported command! Try one of these:"
+    printf '%s\n' "$commands"
+  fi
+}
---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git -C /data/community/.oh-my-comma pull ;
+source ${OH_MY_COMMA_PATH}/install.sh
