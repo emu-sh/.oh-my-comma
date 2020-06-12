daadba81cd83d3e0f61fd18c9824ad57a31b0683
AskAlice <alice@askalice.me>
2020-06-11 19:56:45 -0600

    update scripts

--- install.sh
+++ install.sh
@@ -26,6 +26,10 @@ if [ ! -x "$(command -v powerline-shell)" ]; then
 fi
 
 echo "Installing emu utilities..."
+
+echo "remounting /system as rewritable (until neos 15)"
+mount -o rw,remount /system
+
 if [ -f "$SYSTEM_BASHRC_PATH" ]; then
   echo "Your system /home/.bashrc exists..."
   if grep -q '/home/.bashrc' -e 'source /data/community/.bashrc'
@@ -33,27 +37,25 @@ if [ -f "$SYSTEM_BASHRC_PATH" ]; then
     echo "Found an entry point point for ${COMMUNITY_BASHRC_PATH} in ${SYSTEM_BASHRC_PATH}, skipping changes to /system"
   else
     echo "Your bashrc file is different than the one on the repo. neos 15 will redirect all users to store their bashrc in /data/community"
-    echo "remounting /system as rewritable (until neos 15)"
-    mount -o rw,remount /system
     echo "moving your current bashrc to /data/community"
     mv ${SYSTEM_BASHRC_PATH} ${COMMUNITY_BASHRC_PATH}
     echo "Copying .bashrc that sources local bashrc to system partition (wont be needed in neos 15)"
     cp ${OH_MY_COMMA_PATH}/default-bashrcs/.bashrc-system ${SYSTEM_BASHRC_PATH}
-    echo "Creating a symlink of ${OH_MY_COMMA_PATH} to /home/.config"
-    ln -s ${OH_MY_COMMA_PATH}/.config /home/.config
-    echo "remounting /system as read-only"
-    mount -o r,remount /system
   fi
 else
   echo "remounting /system as rewritable (until neos 15)"
   mount -o rw,remount /system
   echo "Creating a .bashrc in /home/ that sources the community bashrc in /data/community/"
   cp ${OH_MY_COMMA_PATH}/default-bashrcs/.bashrc-system ${SYSTEM_BASHRC_PATH}
+fi
+
+if readlink -f /home/.config != ${OH_MY_COMMA_PATH}/.config
+then
   echo "Creating a symlink of ${OH_MY_COMMA_PATH} to /home/.config"
   ln -s ${OH_MY_COMMA_PATH}/.config /home/.config
-  echo "remounting /system as read-only"
-  mount -o r,remount /system
 fi
+echo "remounting /system as read-only"
+mount -o r,remount /system
 
 #Coping user bashrc, outside of system partition
 if [ -f "$COMMUNITY_BASHRC_PATH" ]; then
@@ -62,17 +64,13 @@ if [ -f "$COMMUNITY_BASHRC_PATH" ]; then
     echo "Skipping community .bashrc installation as it already sources .oh-my-comma's entrypoint"
   else
     echo "Your community bashrc is different than what we've got in this repo... Echoing out our entry point to the bottom of your bashrc in /data/community/.bashrc"
-    printf "$(cat ${OH_MY_COMMA_PATH}/default-bashrcs/.bashrc-community)\n\
-cd /data ; cd /data/openpilot # just in case openpilot is missing, default to /data\n\n\
-# This is your space to configure your terminal to your liking\n\n" >>  ${COMMUNITY_BASHRC_PATH}
+    printf "$(cat ${OH_MY_COMMA_PATH}/default-bashrcs/.bashrc-community)\n" >>  ${COMMUNITY_BASHRC_PATH}
   fi
 else
   echo "Creating the community .bashrc at ${COMMUNITY_BASHRC_PATH}"
   touch ${COMMUNITY_BASHRC_PATH}
   printf '#!/bin/sh\n' >> ${COMMUNITY_BASHRC_PATH}
-  printf "$(cat ${OH_MY_COMMA_PATH}/default-bashrcs/.bashrc-community)\n\
-cd /data ; cd /data/openpilot # just in case openpilot is missing, default to /data\n\n\
-# This is your space to configure your terminal to your liking\n\n" >>  ${COMMUNITY_BASHRC_PATH}
+  printf "$(cat ${OH_MY_COMMA_PATH}/default-bashrcs/.bashrc-community)\n" >>  ${COMMUNITY_BASHRC_PATH}
 fi
 
 #Post-install
