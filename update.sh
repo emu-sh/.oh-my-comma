4c17495f4e4e4d864ce56d6d81bdcb3538d4b2ab
Shane <shane@smiskol.com>
2020-06-13 10:01:13 -0500

    does this fix it?

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+SYSTEM_BASHRC_PATH=/home/.bashrc
+COMMUNITY_PATH=/data/community
+COMMUNITY_BASHRC_PATH=/data/community/.bashrc
+OH_MY_COMMA_PATH=/data/community/.oh-my-comma
+
 git -C /data/community/.oh-my-comma pull
 source ${OH_MY_COMMA_PATH}/install.sh 'update'
