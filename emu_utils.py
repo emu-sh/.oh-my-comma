72e17ad0b611c8a9c1bba15cf089285ad9fcd9a7
Shane <shane@smiskol.com>
2020-06-13 08:40:24 -0500

    test

--- emu_utils.py
+++ emu_utils.py
@@ -1,5 +1,25 @@
 import sys
 
+
+class Commands:
+  # todo: make this a dict and format with a function
+  commands = "- update: updates this tool, requires restart of ssh session\n" \
+             "- pandaflash: flashes panda\n" \
+             "- pandaflash2: flashes panda without make recover\n" \
+             "- debug: debugging tools\n" \
+             "- installfork: Specify the fork URL after. Moves openpilot to openpilot.old"
+
+  debugging_commands = "- controls: logs controlsd to /data/output.log"
+
+
+class Emu:
+  def __init__(self, args):
+    self.args = args
+    self.parse()
+
+  def parse(self):
+    print(sys.argv)
+
+
 if __name__ == "__main__":
-  print('here!')
-  print(sys.argv)
+  emu = Emu(sys.argv[1:])
