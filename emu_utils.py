a640f58579b7faa51d97cfc4fb1110d849eaeed5
Shane <shane@smiskol.com>
2020-06-15 12:29:05 -0500

    debug

--- emu_utils.py
+++ emu_utils.py
@@ -99,7 +99,7 @@ class Emu:
     out_file = '/data/output.log'
 
     f = open(out_file, 'a')
-    r = subprocess.Popen('python /data/openpilot/selfdrive/controls/controlsd.py'.split(), shell=True)
+    r = subprocess.call(['python', '/data/openpilot/selfdrive/controls/controlsd.py'], shell=True)
 
     print('end')
     # print(r)
