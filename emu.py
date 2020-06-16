57512486604510b739343a40926bf75659504a62
Shane <shane@smiskol.com>
2020-06-15 21:04:08 -0500

    test click

--- emu.py
+++ emu.py
@@ -2,16 +2,5 @@ import os
 import click
 
 
-class Repo(object):
-  def __init__(self, home=None, debug=False):
-    self.home = os.path.abspath(home or '.')
-    self.debug = debug
-
-
-@click.group()
-@click.option('--repo-home', envvar='REPO_HOME', default='.repo')
-@click.option('--debug/--no-debug', default=False,
-              envvar='REPO_DEBUG')
-@click.pass_context
-def cli(ctx, repo_home, debug):
-  ctx.obj = Repo(repo_home, debug)
+def cli():
+  print("Hello World")
