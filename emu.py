341203b5ba8bdff3fcac7a1f41c3948d1b0f6679
Shane <shane@smiskol.com>
2020-06-15 21:00:50 -0500

    test click

--- emu.py
+++ emu.py
@@ -1,15 +1,10 @@
 import click
 
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+def cli(debug):
+    click.echo('Debug mode is %s' % ('on' if debug else 'off'))
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-  """Simple program that greets NAME for a total of COUNT times."""
-  for x in range(count):
-    click.echo('Hello %s!' % name)
-
-
-if __name__ == '__main__':
-  hello()
+@cli.command()  # @cli, not @click!
+def sync():
+    click.echo('Syncing')
