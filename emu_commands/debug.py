6a364d369f8c79aab61a60198ee467136f46aee0
Shane <shane@smiskol.com>
2020-06-17 17:30:21 -0500

    debug

--- emu_commands/debug.py
+++ emu_commands/debug.py
@@ -6,7 +6,7 @@ class Debug(CommandBase):
   def __init__(self, description):
     super().__init__(description)
     self.commands = {'controlsd': Command(description='🔬 logs controlsd to /data/output.log',
-                                          flags=[Flag(['log_file'], 'Name of file to save log to', has_value=True)])}
+                                          flags=[Flag(['-f', '--file'], 'Name of file to save log to', has_value=True)])}
 
   def _controlsd(self):
     # if self.next_arg(ingest=False) is None:
@@ -19,6 +19,7 @@ class Debug(CommandBase):
       return
 
     print(flags.log_file)
+    print(type(flags.log_file))
 
     # r = run('pkill -f controlsd')  # terminates file for some reason  # todo: remove me if not needed
     r = kill('selfdrive.controls.controlsd')  # seems to work, some process names are weird
