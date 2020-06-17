60feac3acca7836e2a4edc1890beb67baa6a757b
Shane <shane@smiskol.com>
2020-06-17 17:25:41 -0500

    add log file flag to debug

--- emu_commands/debug.py
+++ emu_commands/debug.py
@@ -1,13 +1,23 @@
-from emu_commands.base import CommandBase, Command
-from py_utils.emu_utils import run, kill, warning
+from emu_commands.base import CommandBase, Command, Flag
+from py_utils.emu_utils import run, kill, warning, error
 from py_utils.emu_utils import OPENPILOT_PATH
 
 class Debug(CommandBase):
   def __init__(self, description):
     super().__init__(description)
-    self.commands = {'controlsd': Command(description='🔬 logs controlsd to /data/output.log')}
+    self.commands = {'controlsd': Command(description='🔬 logs controlsd to /data/output.log',
+                                          flags=[Flag(['log_file'], 'Name of file to save log to', has_value=True)])}
 
   def _controlsd(self):
+    if self.next_arg(ingest=False) is None:
+      error('You must supply command arguments!')
+      self._help('controlsd')
+      return
+    flags, e = self.parse_flags(self.commands['install'].parser)
+    if e is not None:
+      error(e)
+      return
+
     # r = run('pkill -f controlsd')  # terminates file for some reason  # todo: remove me if not needed
     r = kill('selfdrive.controls.controlsd')  # seems to work, some process names are weird
     if r is None:
