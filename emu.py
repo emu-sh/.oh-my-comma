7c4c91946b55dce451f5b75b0e531c097b5cff09
Shane <shane@smiskol.com>
2020-06-17 18:07:31 -0500

    i couldn't figure it out... :(

--- emu.py
+++ emu.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import time
 import sys
-t = time.time()
-# if __package__ is None:
-import sys
-from os import path
-# sys.path.append(path.abspath(path.join(path.dirname(__file__), 'py_utils')))
-# sys.path.append(path.abspath(path.join(path.dirname(__file__), 'emu_commands')))
+if __package__ is None:
+  import sys
+  from os import path
+  sys.path.append(path.abspath(path.join(path.dirname(__file__), 'py_utils')))
+  sys.path.append(path.abspath(path.join(path.dirname(__file__), 'emu_commands')))
 
-from py_utils.emu_utils import BaseFunctions
-from py_utils.emu_utils import OPENPILOT_PATH
-from emu_commands.fork import Fork
-from emu_commands.update import Update
-from emu_commands.panda import Panda
-from emu_commands.debug import Debug
-print(time.time() - t)
+  from py_utils.emu_utils import BaseFunctions
+  from py_utils.emu_utils import OPENPILOT_PATH
+  from emu_commands.fork import Fork
+  from emu_commands.update import Update
+  from emu_commands.panda import Panda
+  from emu_commands.debug import Debug
+
 sys.path.append(OPENPILOT_PATH)  # for importlib
 DEBUG = not path.exists('/data/params/d')
 
