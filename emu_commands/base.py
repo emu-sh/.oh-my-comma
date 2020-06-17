b9bf45d8a4caec4ceb5a4506ca35ea7cc8b01ed5
Shane <shane@smiskol.com>
2020-06-17 16:01:21 -0500

    add base and fork commands

---
+++ emu_commands/base.py
@@ -0,0 +1,71 @@
+from py_utils.colors import COLORS
+from py_utils.emu_utils import ArgumentParser, BaseFunctions, warning, success
+
+class BaseCommand(BaseFunctions):
+  def __init__(self, description):
+    self.description = description
+
+  def start_function_from_str(self, cmd):
+    cmd = '_' + cmd
+    if not hasattr(self, cmd):
+      print('Command has not been implemented yet, please try updating.')
+      return
+    getattr(self, cmd)()  # call command's function
+
+  def parse_flags(self, parser):
+    try:
+      return parser.parse_args(self.args), None
+    except Exception as e:
+      return None, e
+
+  def _help(self, cmd):
+    # if cmd is None:
+    #   self.print_commands(error_msg='You must specify a command to get help with! Some are:')
+    #   return
+    # if cmd not in self.cc.commands:
+    #   self.print_commands(error_msg='Unknown command! Try one of these:')
+    #   return
+
+    description = self.commands[cmd].description
+    print('{}>>  Description: {}{}'.format(COLORS.CYAN, description, COLORS.ENDC))
+
+    flags = self.commands[cmd].flags
+
+    flags_to_print = []
+    if flags is not None and len(flags) > 0:
+      print('{}>>  Flags:{}'.format(COLORS.WARNING, COLORS.ENDC))
+      for flag in flags:
+        aliases = COLORS.SUCCESS + ', '.join(flag.aliases) + COLORS.WARNING
+        flags_to_print.append(COLORS.WARNING + '  - {}: {}'.format(aliases, flag.description) + COLORS.ENDC)
+      print('\n'.join(flags_to_print))
+
+    commands = self.commands[cmd].commands
+    cmds_to_print = []
+    if commands is not None and len(commands) > 0:
+      print('{}>>  Commands:{}'.format(COLORS.OKGREEN, COLORS.ENDC))
+      for cmd in commands:
+        # cmds_to_print.append('  - {}: {}'.format(cmd, commands[cmd].description))
+        # aliases = COLORS.SUCCESS + ', '.join(flag.aliases) + COLORS.WARNING
+        cmds_to_print.append(COLORS.FAIL + '  - {}: {}'.format(cmd, success(commands[cmd].description, ret=True)) + COLORS.ENDC)
+      print('\n'.join(cmds_to_print))
+
+class Flag:
+  def __init__(self, aliases, description, has_value=False):
+    self.aliases = aliases
+    self.description = description
+    self.has_value = has_value
+
+class Command:
+  def __init__(self, description=None, commands=None, flags=None):
+    self.parser = ArgumentParser()
+    self.description = description
+    self.commands = commands
+    self.has_flags = False
+    self.flags = flags
+    if flags is not None:
+      self.has_flags = True
+      for flag in flags:
+        # for each flag, add it as argument with aliases.
+        # if flag.has_value, parse value as string, if not, assume flag is boolean
+        action = 'store_true' if not flag.has_value else None
+        self.parser.add_argument(*flag.aliases, action=action)
---
+++ emu_commands/fork.py
@@ -0,0 +1,90 @@
+import shutil
+from os import path
+from py_utils.colors import COLORS
+from emu_commands.base import BaseCommand, Command, Flag
+from emu_commands.base import BaseCommand, Flag, Command
+from py_utils.emu_utils import run, kill, error, warning, success, verify_fork_url, is_affirmative, ArgumentParser
+from py_utils.emu_utils import SYSTEM_BASHRC_PATH, COMMUNITY_PATH, COMMUNITY_BASHRC_PATH, OH_MY_COMMA_PATH, UPDATE_PATH, OPENPILOT_PATH, EMU_ART
+
+class Fork(BaseCommand):
+  """Make sure to set class.args after init"""
+  def __init__(self, description):
+    super().__init__(description)
+    self.commands = {'install': Command(description='🦉 Whoooose fork do you wanna install?',
+                                        flags=[Flag(['clone_url'], 'URL of fork 🍴 to clone', has_value=True),
+                                               Flag(['-l', '--lite'], 'Clones only the default branch with all commits flattened for quick cloning'),
+                                               Flag(['-b', '--branch'], 'Specify the branch to clone after this flag 🎌', True)])}
+
+  def main(self, args):
+    self.args = args
+    cmd = self.next_arg()
+    if cmd is None:
+      self.print_commands(error_msg='You must specify a command for emu fork. Some options are:')
+      return
+    if cmd not in self.commands:
+      self.print_commands(error_msg='Unknown command! Try one of these:')
+      return
+    self.start_function_from_str(cmd)
+
+  def _install(self):
+    if self.next_arg(ingest=False) is None:  # todo: show install help
+      self._help('install')
+      return
+    flags, e = self.parse_flags(self.commands['install'].parser)
+    if e is not None:
+      error(e)
+      return
+
+    if flags.clone_url is None:
+      error('You must specify a fork URL to clone!')
+      return
+    if not verify_fork_url(flags.clone_url):  # verify we can clone before moving folder!
+      error('The specified fork URL is not valid!')
+      return
+
+    OPENPILOT_TEMP_PATH = '{}.temp'.format(OPENPILOT_PATH)
+    if path.exists(OPENPILOT_TEMP_PATH):
+      warning('{} already exists, should it be deleted to continue?'.format(OPENPILOT_TEMP_PATH))
+      if is_affirmative():
+        shutil.rmtree(OPENPILOT_TEMP_PATH)
+      else:
+        error('Exiting...')
+        return
+
+    # Clone fork to temp folder
+    warning('Fork will be installed to {}'.format(OPENPILOT_PATH))
+    clone_flags = []
+    if flags.lite:
+      warning('- Performing a lite clone! (--depth 1)')
+      clone_flags.append('--depth 1')
+    if flags.branch is not None:
+      warning('- Only cloning branch: {}'.format(flags.branch))
+      clone_flags.append('-b {} --single-branch'.format(flags.branch))
+    if len(clone_flags):
+      clone_flags.append('')
+    try:  # catch ctrl+c and clean up after
+      r = run('git clone {}{} {}'.format(' '.join(clone_flags), flags.clone_url, OPENPILOT_TEMP_PATH))  # clone to temp folder
+    except:
+      r = False
+
+    # If openpilot.bak exists, determine a good non-exiting path
+    # todo: make a folder that holds all installed forks and provide an interface of switching between them
+    bak_dir = '{}.bak'.format(OPENPILOT_PATH)
+    bak_count = 0
+    while path.exists(bak_dir):
+      bak_count += 1
+      bak_dir = '{}.{}'.format(bak_dir, bak_count)
+
+    if r:
+      success('Cloned successfully! Installing fork...')
+      if path.exists(OPENPILOT_PATH):
+        shutil.move(OPENPILOT_PATH, bak_dir)  # move current installation to old dir
+      shutil.move(OPENPILOT_TEMP_PATH, OPENPILOT_PATH)  # move new clone temp folder to main installation dir
+      success("Installed! Don't forget to restart your device")
+    else:
+      error('\nError cloning specified fork URL!', end='')
+      if path.exists(OPENPILOT_TEMP_PATH):  # git usually does this for us
+        error(' Cleaning up...')
+        shutil.rmtree(OPENPILOT_TEMP_PATH)
+      else:
+        print()
