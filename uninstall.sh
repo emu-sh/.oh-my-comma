1ebd3e6fa6ef5071cd51a235ac3e136f545fdc27
Shane <shane@smiskol.com>
2020-06-19 00:07:30 -0500

    add status messages

--- uninstall.sh
+++ uninstall.sh
@@ -5,8 +5,11 @@ SYSTEM_BASHRC_PATH=/home/.bashrc
 COMMUNITY_BASHRC_PATH=/data/community/.bashrc
 OH_MY_COMMA_PATH=/data/community/.oh-my-comma
 
+echo "Deleting $SYSTEM_BASHRC_PATH"
 rm $SYSTEM_BASHRC_PATH
+echo "Deleting $COMMUNITY_BASHRC_PATH"
 rm $COMMUNITY_BASHRC_PATH
+echo "Deleting $OH_MY_COMMA_PATH"
 rm -rf $OH_MY_COMMA_PATH
 
 echo "Uninstalled!"
