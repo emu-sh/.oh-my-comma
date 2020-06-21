20e816f120bb6f264d7238fd185e8f1be3c12615
Shane <shane@smiskol.com>
2020-06-21 02:03:01 -0500

    don't delete system .bashrc and don't delete community .bashrc for now

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/bash
 
-SYSTEM_BASHRC_PATH=/home/.bashrc
 # COMMUNITY_PATH=/data/community  # probably shouldn't remove since it will hold forks in the future
 COMMUNITY_BASHRC_PATH=/data/community/.bashrc
 OH_MY_COMMA_PATH=/data/community/.oh-my-comma
 
-echo "Deleting $SYSTEM_BASHRC_PATH"
-rm $SYSTEM_BASHRC_PATH
-echo "Deleting $COMMUNITY_BASHRC_PATH"
-rm $COMMUNITY_BASHRC_PATH
+#echo "Deleting $COMMUNITY_BASHRC_PATH"
+#rm $COMMUNITY_BASHRC_PATH
 echo "Deleting $OH_MY_COMMA_PATH"
 rm -rf $OH_MY_COMMA_PATH
 
