c8bb4839f9d2e3d023bb0ef52eb8f9646bbd0082
Shane <shane@smiskol.com>
2020-06-21 01:42:37 -0500

    does this fix it?

--- update.sh
+++ update.sh
@@ -20,4 +20,4 @@ printf "Version \033[92m${PREV_VERSION}\033[0m | branch \033[92m${PREV_BRANCH}\0
 printf "Version \033[92m${CURRENT_VERSION}\033[0m | branch \033[92m${CURRENT_BRANCH}\033[0m | remote \033[92m${CURRENT_REMOTE}\033[0m\n"
 echo "======= TO ======="
 
-exec $SHELL # auto source
+#exec $SHELL  # auto source
