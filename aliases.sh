5b0a5f7b712f6b7a04fb7573c367b79a8aa38cb5
Shane <shane@smiskol.com>
2020-06-22 17:39:45 -0500

    last commit, remove from aliases

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,3 @@
 alias ll="ls -lhA"
 alias pf="emu panda flash"
 alias controlsd="emu debug controlsd"
-alias battery="dumpsys batterymanager"
-alias emu_uninstall="sh /data/community/.oh-my-comma/uninstall.sh"
