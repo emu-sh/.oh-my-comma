8bc28c08ff6e3efe85d4e389ea6ed48668bbaa21
AskAlice <alice@section.io>
2020-06-23 09:45:37 -0600

    uninstall the entrypoint script

--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,7 @@ OH_MY_COMMA_PATH=/data/community/.oh-my-comma
 
 #echo "Deleting $COMMUNITY_BASHRC_PATH"
 #rm $COMMUNITY_BASHRC_PATH
+sed -i.bak.$(date +"%Y-%m-%d-%T") -e  '/^## BEGIN \.oh-my-comma magic ###/,/^### End of \.oh-my-comma magic ###/d' ${COMMUNITY_BASHRC_PATH}
 echo "Deleting $OH_MY_COMMA_PATH"
 rm -rf $OH_MY_COMMA_PATH
 
