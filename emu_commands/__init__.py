9f95b8154b667906208878c5f646f25466c5ee86
Shane Smiskol <shane@smiskol.com>
2020-07-05 08:09:51 -0500

    disable all commands but fork

--- emu_commands/__init__.py
+++ emu_commands/__init__.py
@@ -1,9 +1,10 @@
 from emu_commands.fork import Fork
-from emu_commands.update import Update
-from emu_commands.panda import Panda
-from emu_commands.debug import Debug
-from emu_commands.device import Device
-from emu_commands.uninstall import Uninstall
+# from emu_commands.update import Update
+# from emu_commands.panda import Panda
+# from emu_commands.debug import Debug
+# from emu_commands.device import Device
+# from emu_commands.uninstall import Uninstall
 
 
-EMU_COMMANDS = [Fork(), Update(), Panda(), Debug(), Device(), Uninstall()]
+# EMU_COMMANDS = [Fork(), Update(), Panda(), Debug(), Device(), Uninstall()]
+EMU_COMMANDS = [Fork()]
