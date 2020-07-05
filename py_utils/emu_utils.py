236f82dd7610ba0c273eb10e83c100eb06cc7d96
Shane Smiskol <shane@smiskol.com>
2020-07-05 08:55:38 -0500

    debug

--- py_utils/emu_utils.py
+++ py_utils/emu_utils.py
@@ -92,7 +92,9 @@ def check_output(cmd, cwd=None):
   try:
     return Output(subprocess.check_output(cmd, cwd=cwd, stderr=subprocess.STDOUT, encoding='utf8'))
   except subprocess.CalledProcessError as e:
-    return Output(s=False if e.output is None else True, e=e.output)
+    if e.output is None:
+      return Output(s=False, e=e)  # command failed to execute
+    return Output(e=e.output)  # command executed but it resulted in error
 
 
 def run(cmd, out_file=None):
