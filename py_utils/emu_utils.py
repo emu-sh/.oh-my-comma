1e9276d97e0991e540d222445f10e52506ad6629
Shane Smiskol <shane@smiskol.com>
2020-07-05 08:52:38 -0500

    debug

--- py_utils/emu_utils.py
+++ py_utils/emu_utils.py
@@ -92,7 +92,7 @@ def check_output(cmd, cwd=None):
   try:
     return Output(subprocess.check_output(cmd, cwd=cwd, stderr=subprocess.STDOUT, encoding='utf8'))
   except subprocess.CalledProcessError as e:
-    return Output(s=False, e=e)
+    return Output(s=False, e=e.output)
 
 
 def run(cmd, out_file=None):
