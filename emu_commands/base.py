e8bc80db8ab15e8542a277f3cf6bd5c03e9d1131
Shane Smiskol <shane@smiskol.com>
2020-07-05 08:33:12 -0500

    is this better?

--- emu_commands/base.py
+++ emu_commands/base.py
@@ -79,7 +79,8 @@ class Command:
         # if flag.has_value, parse value as string, if not, assume flag is boolean
         parser_args = {}
         if not flag.required and flag.dtype not in ['bool']:
-          parser_args['nargs'] = '?'
+          # parser_args['nargs'] = '?'
+          parser_args['required'] = True
 
         if flag.dtype != 'bool':
           parser_args['action'] = 'store'
