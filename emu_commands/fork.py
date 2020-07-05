3450d08c6146bc044d524e305c4e8236e37dda23
Shane Smiskol <shane@smiskol.com>
2020-07-05 07:33:04 -0500

    debug

--- emu_commands/fork.py
+++ emu_commands/fork.py
@@ -71,6 +71,9 @@ class Fork(CommandBase):
 
   def _init(self):
     if self.fork_params.get('setup_complete'):
+      if os.path.exists(COMMAAI_PATH):  # ensure we're really set up (directory got deleted?)
+        r = check_output(['git', '-C', COMMAAI_PATH, 'branch'])
+        print('param set but checking: {}'.format(r))
       return True  # already set up
     info('To set up emu fork management we will clone commaai/openpilot into /data/community/forks')
     info('Please confirm you would like to continue')
