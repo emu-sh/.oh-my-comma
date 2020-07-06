061370a51c97da0aa5d7e31591dcf952d7764202
Shane Smiskol <shane@smiskol.com>
2020-07-06 07:40:35 -0500

    revert, optional should be red?

--- commands/base.py
+++ commands/base.py
@@ -43,10 +43,9 @@ class CommandBase(BaseFunctions):
     if flags is not None and len(flags) > 0:
       print(leading + '{}>>  Arguments 💢:{}'.format(COLORS.WARNING, COLORS.ENDC))
       for flag in flags:
-        aliases = COLORS.SUCCESS + ', '.join(flag.aliases)
+        aliases = COLORS.SUCCESS + ', '.join(flag.aliases) + COLORS.WARNING
         if not flag.required and '-' not in aliases:
-          aliases += ' (optional)'
-        aliases += COLORS.WARNING
+          aliases += COLORS.RED + ' (optional)' + COLORS.WARNING
         flags_to_print.append(leading + COLORS.WARNING + '  - {}: {}'.format(aliases, flag.description) + COLORS.ENDC)
       print('\n'.join(flags_to_print))
 
