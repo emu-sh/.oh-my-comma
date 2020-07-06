f17db7d79c1885e37678f4814f6a637fc4fa9800
Shane Smiskol <shane@smiskol.com>
2020-07-06 08:36:14 -0500

    so this should work?

--- commands/base.py
+++ commands/base.py
@@ -41,16 +41,15 @@ class CommandBase(BaseFunctions):
 
     flags_to_print = []
     if flags is not None and len(flags) > 0:
-      usage_req = [f.aliases[0] for f in flags if f.required and len(f.aliases) == 1]  # if required or non-required non-positional
-      usage_non_req = [f.aliases[0] for f in flags if not f.required and len(f.aliases) == 1]
-      # print([(f.aliases, f.required, len(f.aliases)) for f in flags])
+      usage_req = [f.aliases[0] for f in flags if f.required and len(f.aliases) == 1]  # if required
+      usage_non_req = [f.aliases[0] for f in flags if not f.required and len(f.aliases) == 1]  # if non-required non-positional
       if len(usage_req) > 0 or len(usage_non_req) > 0:
         usage_req = ['[{}]'.format(u) for u in usage_req]
-        # usage_req = ['emu', self.name, cmd] + usage_req
         usage_non_req = ['({})'.format(u) for u in usage_non_req]
+        usage_req = ['emu', self.name, cmd] + usage_req + usage_non_req
         print(usage_req)
         print(usage_non_req)
-        # print(leading + COLORS.WARNING + '>>  Usage:{} {}'.format(COLORS.OKGREEN, ' '.join(usage)) + COLORS.ENDC)
+        print(leading + COLORS.WARNING + '>>  Usage:{} {}'.format(COLORS.OKGREEN, ' '.join(usage)) + COLORS.ENDC)
       print(leading + COLORS.WARNING + '>>  Arguments 💢:' + COLORS.ENDC)
       for flag in flags:
         aliases = COLORS.SUCCESS + ', '.join(flag.aliases) + COLORS.WARNING
