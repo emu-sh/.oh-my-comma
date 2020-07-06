5be3fe2d6b3ebe0c5463ea8f33e6d039ba742c37
Shane Smiskol <shane@smiskol.com>
2020-07-06 08:12:25 -0500

    two spaces indentation ftw

--- commands/__init__.py
+++ commands/__init__.py
@@ -2,15 +2,14 @@ import os
 import importlib
 from py_utils.emu_utils import error
 
-
 EMU_COMMANDS = []
 for module_name in os.listdir(os.path.dirname(__file__)):
-    if module_name.endswith('.py') or module_name == '__pycache__':
-        continue
-    try:
-      module = importlib.import_module('commands.{}'.format(module_name))
-      module = getattr(module, module_name.title())()
-      EMU_COMMANDS.append(module)
-    except Exception as e:
-      error('Error loading {} command, please try updating!'.format(module_name))
-      error(e)
+  if module_name.endswith('.py') or module_name == '__pycache__':
+    continue
+  try:
+    module = importlib.import_module('commands.{}'.format(module_name))
+    module = getattr(module, module_name.title())()
+    EMU_COMMANDS.append(module)
+  except Exception as e:
+    error('Error loading {} command, please try updating!'.format(module_name))
+    error(e)
