248192a8f566ed6222cea827585e6a817372bee7
Shane Smiskol <shane@smiskol.com>
2020-07-06 06:57:22 -0500

    test

--- emu_commands/__init__.py
+++ emu_commands/__init__.py
@@ -4,24 +4,27 @@ from py_utils.emu_utils import error
 from os.path import dirname, basename, isfile, join
 
 import os
-for module in os.listdir(os.path.dirname(__file__)):
-    if module == '__init__.py' or module[-3:] == '.py':
+for module_name in os.listdir(os.path.dirname(__file__)):
+    if module_name.endswith('.py'):
         continue
-    print(module)
-    # __import__(module[:-3], locals(), globals())
-raise Exception
-EMU_COMMANDS = []
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
-__all__.remove('base')
-print(__all__)
-print([basename(f)[:-3] for f in modules])
-
-for module_name in __all__:
-  try:
     module = importlib.import_module('emu_commands.{}'.format(module_name))
-    module = getattr(module, module_name.title())()
-    EMU_COMMANDS.append(module)
-  except Exception as e:
-    error('Error loading {} command, please try updating!'.format(module_name))
-    error(e)
+    print(dir(module))
+    print(module_name)
+
+    # __import__(module[:-3], locals(), globals())
+# raise Exception
+# EMU_COMMANDS = []
+# modules = glob.glob(join(dirname(__file__), "*.py"))
+# __all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+# __all__.remove('base')
+# print(__all__)
+# print([basename(f)[:-3] for f in modules])
+#
+# for module_name in __all__:
+#   try:
+#     module = importlib.import_module('emu_commands.{}'.format(module_name))
+#     module = getattr(module, module_name.title())()
+#     EMU_COMMANDS.append(module)
+#   except Exception as e:
+#     error('Error loading {} command, please try updating!'.format(module_name))
+#     error(e)
