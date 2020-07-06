a0e3829fb3151223da2cf456cc6a29fd626a00f0
Shane Smiskol <shane@smiskol.com>
2020-07-06 04:57:15 -0500

    print branches

--- emu_commands/fork.py
+++ emu_commands/fork.py
@@ -76,6 +76,11 @@ class Fork(CommandBase):
     success('Installed forks:')
     for fork in installed_forks:
       print(' - {}{}{}'.format(COLORS.OKBLUE, fork, COLORS.ENDC))
+      success(' Branches:')
+      for idx, branch in enumerate(installed_forks[fork]['installed_branches']):
+        print('  - {}{}{}'.format(COLORS.RED, branch, COLORS.ENDC))
+        if idx == 4:
+          print('  - {}...{}'.format(COLORS.RED, COLORS.ENDC))
 
   def _switch(self):
     if not self._init():
