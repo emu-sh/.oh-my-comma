402b4edb464ba905c1f314d50b9092cda032703d
Shane Smiskol <shane@smiskol.com>
2020-07-07 22:16:12 -0500

    add emoji to reboot and shut down

--- commands/device/__init__.py
+++ commands/device/__init__.py
@@ -12,10 +12,12 @@ class Device(CommandBase):
                      'shutdown': Command(description='🔌 safely shutdown your device')}
 
   def _reboot(self):
-    run('am start -a android.intent.action.REBOOT')
+    check_output('am start -a android.intent.action.REBOOT')
+    print('See you in a bit! 🔄')
 
   def _shutdown(self):
-    run('am start -n android/com.android.internal.app.ShutdownActivity')
+    check_output('am start -n android/com.android.internal.app.ShutdownActivity')
+    success('Goodnight! 🌙')
 
   def _battery(self):
     r = check_output('dumpsys batterymanager')
