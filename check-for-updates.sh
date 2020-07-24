2240e54fedeece957f1226d1effae492bf9c35f9
AskAlice <alice@askalice.me>
2020-07-24 08:00:50 -0600

    update branch status

--- check-for-updates.sh
+++ check-for-updates.sh
@@ -60,7 +60,8 @@ if [ $OMC_LOCAL != $OMC_REMOTE ]; then
   if [[ "$OMC_DISABLE_UPDATE_PROMPT" = true ]]; then
       emu update
   else
-      echo $(git status | head -n 2 | tail -n 1)
+      echo "[emu.sh] Current .oh-my-comma branch: $(git branch | head -n 1)"
+      echo "$(git status | head -n 2 | tail -n 1)"
       # input sink to swallow all characters typed before the prompt
       # and add a newline if there wasn't one after characters typed
       read -r -p "[emu.sh] Update .oh-my-comma? [Y/n] " option
