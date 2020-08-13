2929ed8a47bbc40281c0908914839ffa162ee42c
Shane Smiskol <shane@smiskol.com>
2020-08-12 22:06:52 -0500

    use info

--- py_utils/emu_utils.py
+++ py_utils/emu_utils.py
@@ -136,7 +136,7 @@ def kill(procname):
 
 def is_affirmative():
   i = None
-  print(COLORS.WARNING, end='')
+  print(COLORS.INFO, end='')
   while i not in ['y', 'n', 'yes', 'no', 'sure', '']:
     i = input('[Y/n]: ').lower().strip()
   print(COLORS.ENDC)
