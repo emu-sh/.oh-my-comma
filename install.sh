aa27dfc83059af11218379a8d7bde0834629e55a
Shane Smiskol <shane@smiskol.com>
2020-09-28 20:48:02 -0500

    this?

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ GIT_BRANCH_NAME=master
 GIT_REMOTE_URL=https://github.com/emu-sh/.oh-my-comma.git
 OMC_VERSION=0.1.9
 
-function update_print() {  # only prints if not updating
+update_print() {  # only prints if not updating
   if [ "$update" != true ]; then
     echo "$1"
   fi
