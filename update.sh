a305ce6038ca233e457d889a7d70d63d86cb9660
Shane Smiskol <shane@smiskol.com>
2020-09-28 22:15:27 -0500

    only show if updated this time

--- update.sh
+++ update.sh
@@ -21,7 +21,9 @@ printf "Version \033[92m${CURRENT_VERSION}\033[0m | branch \033[92m${CURRENT_BRA
 echo "======= TO ======="
 
 if git -C $OH_MY_COMMA_PATH log --stat -1 | grep -q 'default-bashrcs/.bashrc-community'; then
-  printf "\n\33[38;5;190mThe default .bashrc has been updated! The change has not been applied to retain your changes.\nTo update and reset your .bashrc, run the command:\n"
-  printf "\033[92mcp -fr /data/community/.oh-my-comma/default-bashrcs/.bashrc-community /data/community/.bashrc\n"
-  printf "This will wipe any custom changes you've made!\033[0m\n"
+  if [ ${PREV_VERSION} = ${CURRENT_VERSION} ]; then
+    printf "\n\33[38;5;190mThe default .bashrc has been updated! The change has not been applied to retain your changes.\nTo update and reset your .bashrc, run the command:\n"
+    printf "\033[92mcp -fr /data/community/.oh-my-comma/default-bashrcs/.bashrc-community /data/community/.bashrc\n"
+    printf "This will wipe any custom changes you've made!\033[0m\n"
+  fi
 fi
