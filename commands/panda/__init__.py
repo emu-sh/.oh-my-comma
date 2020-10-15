45c4de26555e2c0cd4ea9e4c7a15bae0986adecc
Shane Smiskol <shane@smiskol.com>
2020-10-15 13:16:50 -0500

    make methods static and remove unused imports

--- commands/panda/__init__.py
+++ commands/panda/__init__.py
@@ -5,6 +5,7 @@ from commands.base import CommandBase, Command
 from py_utils.emu_utils import run, error
 from py_utils.emu_utils import OPENPILOT_PATH
 
+
 class Panda(CommandBase):
   def __init__(self):
     super().__init__()
@@ -14,12 +15,14 @@ class Panda(CommandBase):
     self.commands = {'flash': Command(description='🐼 flashes panda with make recover (usually works with the C2)'),
                      'flash2': Command(description='🎍 flashes panda using Panda module (usually works with the EON)')}
 
-  def _flash(self):
+  @staticmethod
+  def _flash():
     r = run('make -C {}/panda/board recover'.format(OPENPILOT_PATH))
     if not r:
       error('Error running make command!')
 
-  def _flash2(self):
+  @staticmethod
+  def _flash2():
     if not run('pkill -f boardd'):
       error('Error killing boardd! Is it running? (continuing...)')
     importlib.import_module('panda', 'Panda').Panda().flash()
--- commands/update/__init__.py
+++ commands/update/__init__.py
@@ -2,12 +2,14 @@ from commands.base import CommandBase
 from py_utils.emu_utils import run, error
 from py_utils.emu_utils import UPDATE_PATH
 
+
 class Update(CommandBase):
   def __init__(self):
     super().__init__()
     self.name = 'update'
     self.description = '🎉 Updates this tool'
 
-  def _update(self):
+  @staticmethod
+  def _update():
     if not run(['sh', UPDATE_PATH]):
       error('Error updating!')
