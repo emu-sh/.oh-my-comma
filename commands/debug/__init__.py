ee1d1399ed2d39a4cf1e641bf132b427120993af
Shane Smiskol <shane@smiskol.com>
2021-04-15 20:36:49 -0500

    debug

--- commands/debug/__init__.py
+++ commands/debug/__init__.py
@@ -14,10 +14,10 @@ class Debug(CommandBase):
     self.commands = {'controlsd': Command(description='🔬 logs controlsd to /data/output.log by default',
                                           flags=[Flag(['-o', '--output'], 'Name of file to save log to', dtype='str')]),
                      'reload': Command(description='🔄 kills the current openpilot session and restarts it (all without rebooting)')}
-    self.default_path = '/data/output.log'
 
-  def _reload(self):
-    info('This will kill the current openpilot tmux session, create a new session adding its PID to the app cpuset, and relaunch openpilot.')
+  @staticmethod
+  def _reload():
+    info('This will kill the current openpilot tmux session, set up a new one properly, and relaunch openpilot.')
     info('Confirm you would like to continue')
     if not is_affirmative():
       error('Aborting!')
@@ -38,17 +38,9 @@ class Debug(CommandBase):
       success('Succesfully started a new tmux session for openpilot!')
       success('Type tmux a to attach to it')
 
-
-
-
-  def _kill_and_create_tmux_session(self):
-    r = check_output(['tmux', 'new', '-s', 'comma'])
-    return r.success
-
-
   def _controlsd(self):
+    out_file = '/data/output.log'
     flags = self.get_flags('controlsd')
-    out_file = self.default_path
     if flags.output is not None:
       out_file = flags.output
     # r = run('pkill -f controlsd')  # terminates file for some reason  # todo: remove me if not needed
