699977ec870d8cea9b53b2f56a3b433e0b93e8cc
AskAlice <alice@askalice.me>
2021-08-21 18:29:46 -0600

    update emu.sh

--- emu.sh
+++ emu.sh
@@ -2,7 +2,7 @@
 SYSTEM_BASHRC_PATH=/home/.bashrc
 export COMMUNITY_PATH=/data/community
 export COMMUNITY_BASHRC_PATH=${COMMUNITY_PATH}/.bashrc
-export OH_MY_COMMA_PATH="$(dirname $(readlink -f $0))"
+export OH_MY_COMMA_PATH="/data/community/.oh-my-comma"
 
 source ${OH_MY_COMMA_PATH}/aliases.sh
 
