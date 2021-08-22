d354e37c2806f66f845d61b72d702bf15ff83971
AskAlice <alice@askalice.me>
2021-08-21 18:06:02 -0600

    test

--- install-oh-my-zsh.zsh
+++ install-oh-my-zsh.zsh
@@ -9,19 +9,19 @@ if [ ! -f /data/community/.p10k.zsh ]; then
     fi
     ln -s /data/community/.p10k.zsh ~/.p10k.zsh
 fi
-curl https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh --output /data/community/install-ohmyzsh.sh
+wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O /data/community/install-ohmyzsh.sh
 chmod +x /data/community/install-ohmyzsh.sh
 ZSH="/data/community/.oh-my-zsh" sh /data/community/install-ohmyzsh.sh --unattended
 rm /data/community/install-ohmyzsh.sh
 mv ~/.zshrc /data/community/.zshrc
 ln -s /data/community/.zshrc ~/.zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
-sed -i '/^# Path to your oh-my-zsh installation./d' ~/.zshrc
-sed -i '/^export ZSH=/d' ~/.zshrc
-sed -i '1iexport ZSH="/data/community/.oh-my-zsh"\nexport ZSH_CUSTOM="/data/community/.oh-my-zsh/custom"\nexport HISTFILE=/data/community/.zsh_history\nZDOTDIR=/data/community\n' ~/.zshrc
 rm -rf /home/.oh-my-zsh
 ln -s /data/community/.oh-my-zsh /home/.oh-my-zsh
 ln -s /data/community/.zsh_history ~/.zsh_history
+sed -i '/^# Path to your oh-my-zsh installation./d' ~/.zshrc
+sed -i '/^export ZSH=/d' ~/.zshrc
+sed -i '1iexport ZSH="/data/community/.oh-my-zsh"\nexport ZSH_CUSTOM="/data/community/.oh-my-zsh/custom"\nexport HISTFILE=/data/community/.zsh_history\nZDOTDIR=/data/community\n' ~/.zshrc
 echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
 sed -i 's/^ZSH_THEME=".\+"$/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g' ~/.zshrc
 mv ~/.zshrc /data/community/.zshrc
